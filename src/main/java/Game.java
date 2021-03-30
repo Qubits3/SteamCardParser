18c6830a523e44e6f8be1a6ef8863e6c07a1a9b2
Metin Yıldıran <metin.1464@gmail.com>
2021-03-30 23:18:06 +0300

    Getting game info successfully.

---
+++ src/main/java/Game.java
@@ -0,0 +1,36 @@
+public class Game {
+
+    private String name;
+    private String discount;
+    private String price;
+
+    public Game(String name, String discount, String price) {
+        this.name = name;
+        this.discount = discount;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
--- src/main/java/SteamPriceParser.java
+++ src/main/java/SteamPriceParser.java
@@ -1,16 +1,26 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SteamPriceParser {
 
     private ArrayList<String> links;
     private String fullSteamHTML;
+
+    private final String COMMENT = "<!--[if lte IE 7]>\n" +
+            "<style type=\"text/css\">\n" +
+            ".game_area_purchase_game_dropdown_right_panel .btn_addtocart { float: none; }\n" +
+            "</style>\n" +
+            "<![endif]-->";
+    private final String BTN_ADD_TO_CART = "<div class=\"btn_addtocart\">";
+    private final String DISCOUNT_PCT = "<div class=\"discount_pct\">";
+    private final String DISCOUNT_ORIGINAL_PRICE = "<div class=\"discount_original_price\">";
+    private final String DISCOUNT_FINAL_PRICE = "<div class=\"discount_final_price\">";
 
     public SteamPriceParser(ArrayList<String> links){
         this.links = links;
@@ -35,9 +45,18 @@ public class SteamPriceParser {
             fullSteamHTML = driver.getPageSource();
 
             Document doc = Jsoup.parse(fullSteamHTML);
-            String discounts = doc.body().child(0).child(6).child(3).child(2).child(4).child(5).child(1).child(0).child(0).child(0).toString();
+            String purchaseArea = doc.body().select("#game_area_purchase").toString();
 
-            System.out.println(discounts);
+            // ToDo: Buraları jsoup parser a atıp öyle ayır
+            String cleanPurchaseArea = purchaseArea.substring(purchaseArea.indexOf(COMMENT), purchaseArea.indexOf(BTN_ADD_TO_CART));
+            String gameName = cleanPurchaseArea.substring(cleanPurchaseArea.indexOf("<h1>") + 8, cleanPurchaseArea.indexOf("</h1>"));
+            String discount = cleanPurchaseArea.substring(cleanPurchaseArea.indexOf(DISCOUNT_PCT) + 34, cleanPurchaseArea.indexOf("<div class=\"discount_prices\">") - 20);
+            String discountFinalPrice = cleanPurchaseArea.substring(cleanPurchaseArea.indexOf(DISCOUNT_FINAL_PRICE) + 43, cleanPurchaseArea.length() - 46);
+
+            System.out.println(cleanPurchaseArea);
+            System.out.println(gameName);
+            System.out.println(discount);
+            System.out.println(discountFinalPrice);
 
 //            if (discounts.equals("")){
 //                System.out.println(link + " indirim falan yok kardeş");
@@ -50,6 +69,13 @@ public class SteamPriceParser {
 
         driver.close();
         driver.quit();
+
+
     }
 
+    private static void printArray(List<String> arrayList) {
+        for (String x : arrayList) {
+            System.out.println(x.toString());
+        }
+    }
 }
